2a13be6a9fa8d35a1eed629b5b47782d9c753548
Meenal-goel <meenalgoel02@gmail.com>
2018-06-06 22:28:13 +0530

    first change

---
+++ printfun.py
@@ -0,0 +1,61 @@
+#program in python to print anything we wish on screen
+
+print('''Welcome to "PYTHON CLASS"''')
+print(2,3,4)
+print(2,3,4,sep='$')
+print("hey \t there")
+print(50*"-")
+
+print("\n")
+
+#program in python to join two strings using '+'
+
+a="Acad"
+b="View"
+print(a+b)
+print(50*"-")
+
+print("\n")
+
+#program in python to take input of 3 variables x,y and z .Print their values on screen
+print("\n")
+x=input("enter the value of first variable")
+y=input("enter the value of second variable")
+z=input("enter the value of third variable")
+print(x ,"\t", y ,"\t" , z)
+print(50*"-")
+
+print("\n")
+
+#program in python to print "Let's get started" on screen
+
+print('''"Let's get started" ''')
+print(50*"-")
+
+print("\n")
+
+#program in python to print the following values using placeholders:
+#s="Acadview"
+#course="Python"
+#fees=5000
+
+s='''"Acadview"'''
+course='''"Python"'''
+fees=5000
+print("%s \n %s \n %s "%(s,course,fees))
+print(50*"-")
+
+print("\n")
+
+#program to do interesting exercise:
+#name="Tony Stark"
+#salary=1000000
+#print("%s""%d"%(__,__))
+
+name='''"Tony Stark"'''
+salary= 1000000
+print("%s""%d"%(name,salary))
+print(50*"-")
+
+print("\n")
+
